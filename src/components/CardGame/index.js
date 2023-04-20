2291cd17ca85fef21d0d89d1ae832a9dee4d5ca5
Rodrigo <rodrigoscrew@hotmail.com>
2023-04-20 16:12:42 -0300

    :tada: Create cardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function CardGame () {
+    return `
+    <article class="card-game">
+        <img src="images/alura-pixel.png"
+    </article>    
+    `;
+}
+
+
+
+export default CardGame;
