fddaf7fd7d15532edf584519d41ae20caf4ee35b
Rodrigo <rodrigoscrew@hotmail.com>
2023-04-24 15:39:55 -0300

    :construction: Create Board object without CSS

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
-import "./src/styles/settings/colors.css"
-import "./src/styles/generic/reset.css"
-import "./src/styles/elements/base.css"
-import CardGame from "./src/components/CardGame";
+import "./src/styles/settings/colors.css";
+import "./src/styles/generic/reset.css";
+import "./src/styles/elements/base.css";
+import BoardGame from "./src/components/objects/BoradGame";
+
 
 const $root = document.querySelector("#root");
-const $htmlCardGame = CardGame()
+const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame)
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 function CardGame () {
     return `
     <article class="card-game">
-        <img src="images/alura-pixel.png"
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
     </article>    
     `;
 }
